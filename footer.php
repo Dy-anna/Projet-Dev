1781d4f71d38402361e7c973a69744167af2d1cd
Dayana Okandza <164897886+Dy-anna@users.noreply.github.com>
2024-06-05 10:32:21 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,78 @@
+<div>
+	<div class="newsletter-section">
+    	<div class="newsletter-content">
+			<h2><i class='bx bx-envelope'></i>Newsletter Parfum</h2>
+			<h3>Recevez les nouveautés, actualités et offres Girly's Perfums par email.</h3><br>
+        	<p>En m'inscrivant, j'accepte de recevoir par email mon code de réduction ainsi que les offres et actualités de Girly's Perfums et je déclare avoir plus de 16 ans. Je consens au traitement de mes <a href="Utilisation de vos données et vos droits.html">données personnelles</a> et accepte la <a href="Politique de données personnelles et cookies.html">politique de confidentialité</a>.</p>
+    	</div>
+			<div class="newsletter-form">
+        		<form>
+            		<label for="email">Saisissez votre e-mail<span>*</span></label>
+            		<input type="email" id="email" name="email" placeholder="Ex: pierre.dubois@gmail.com" required>
+            		<button type="submit">S'inscrire</button>
+            		<p><span>*</span>Champs obligatoires</p>
+        		</form>
+    		</div>
+		</div>
+	</div>
+</div><br>
+<footer>
+	<div class="footer-content">
+		<div class="img-box">
+			<img src="Logo2.png">
+		</div>
+		<div class="inner-footer">
+			<div class="card">
+				<h3>Besoin d'aide ?</h3>
+				<ul>
+					<li>FAQ et Contact</li>
+					<li>Expériences Girly's Perfums</li>
+					<li>Mon compte</li>
+					<li>Echange, Retours, Et Remboursements</li>
+					<li>Livraison</li>
+					<li>Assistance</li>
+				</ul>
+			</div><br>
+			<div class="card">
+				<h3>Nous</h3>
+				<ul>
+					<li>A propos de Girly's Perfums</li>
+					<li>Nos engagements</li>
+					<li>Environnement</li>
+					<li>Salle de presse</li>
+					<li>Blogger</li>
+					<li>Règlement sur les services</li>
+				</ul>
+			</div><br>
+			<div class="card">
+				<h3>Newsletter Parfums</h3>
+				<p>Abonnez-vous à notre Newsletter</p>
+				<div class="social-links">
+					<i class='bx bxl-tiktok'></i><i class='bx bxl-instagram' ></i><i class='bx bxl-pinterest' ></i><i class='bx bxl-youtube' ></i>
+				</div>
+			</div><br>
+			<div class="card">
+				<h3>Conditions</h3>
+				<ul>
+					<li>Mentions légales ofrres</li>
+					<li>Données Personnelles</li>
+					<li>Mentions légales et CDU</li>
+					<li>CGV</li>
+					<li>CGU avis</li>
+				</ul>
+			</div><br>
+			<div class="card">
+				<h3>Moyens De Paiments Acceptés</h3>
+				<ul>
+					<li><i class='bx bxl-visa' ></i>Visa</li>
+					<li><i class='bx bxl-mastercard'></i>Mastercard</li>
+					<li><i class='bx bxl-paypal' ></i>Paypal</li>
+					<li><i class='bx bx-credit-card'></i>Carte Bancaire</li>
+				</ul>
+			</div>
+		</div>
+		<div class="bottom-footer">
+			<p>Copyright &copy; 2024 by Girly's Perfums | Tous Droits Réservés.</p>
+		</div>
+	</div>
+</footer>
